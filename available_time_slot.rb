e13e20143f47c1e14b292b7cf1110097862a6d28
Charlie LIM <charlie@hosman.co>
2024-03-19 22:04:48 +0100

    opti

--- available_time_slot.rb
+++ available_time_slot.rb
@@ -15,25 +15,28 @@ end
 
 def find_available_slots(serialized_agenda, duration)
   available_slots = []
-  current_time = serialized_agenda.first['start'].to_date
+  start_time = serialized_agenda.first['start'].to_date
 
-  until current_time == serialized_agenda.last['end'].to_date.next
-    start_of_day = DateTime.new(current_time.year, current_time.month, current_time.day, 9, 0, 0)
-    end_of_day = DateTime.new(current_time.year, current_time.month, current_time.day, 18, 0, 0)
-    available_slots.concat(find_slots_for_day(serialized_agenda, start_of_day, end_of_day, duration))
-    current_time = current_time.next
+  until start_time == serialized_agenda.last['end'].to_date.next
+    daily_agenda = serialized_agenda.select do |event|
+      start_date = event['start'].to_date
+      start_date == start_time
+    end
+    available_slots.concat(find_slots_for_day(daily_agenda, start_time, duration))
+    start_time = start_time.next
   end
   available_slots
 end
 
-def find_slots_for_day(events, start_of_day, end_of_day, duration)
+def find_slots_for_day(events, start_time, duration)
   available_slots = []
+  start_of_day = DateTime.new(start_time.year, start_time.month, start_time.day, 9, 0, 0)
+  end_of_day = DateTime.new(start_time.year, start_time.month, start_time.day, 18, 0, 0)
   previous_event_end = start_of_day
+
   events.each do |event|
     start_time = event['start']
     end_time = event['end']
-
-    next if start_time >= end_of_day || end_time <= start_of_day
 
     if start_time > previous_event_end
       available_duration = (start_time - previous_event_end) * 24 * 60
@@ -66,15 +69,15 @@ def serialize_agenda(agenda)
   end
 end
 
-# agenda1_path = 'input_andy.json'
-# agenda2_path = 'input_sandra.json'
-# serialize_agenda = serialize_data(agenda1_path, agenda2_path)
+agenda1_path = 'input_andy.json'
+agenda2_path = 'input_sandra.json'
+serialize_agenda = serialize_data(agenda1_path, agenda2_path)
 
-# if serialize_agenda
-#  available_slots = find_available_slots(serialize_agenda, 60) # Durée de 1 heure
-#  available_slots.each do |slot|
-#    puts "#{slot[0].strftime('%Y-%m-%d %H:%M')} - #{slot[1].strftime('%Y-%m-%d %H:%M')}"
-#  end
-# else
-#  puts "Erreur lors de l'ouverture ou de l'analyse des fichiers JSON."
-# end
+if serialize_agenda
+  available_slots = find_available_slots(serialize_agenda, 60) # Durée de 1 heure
+  available_slots.each do |slot|
+    puts "#{slot[0].strftime('%Y-%m-%d %H:%M')} - #{slot[1].strftime('%Y-%m-%d %H:%M')}"
+  end
+else
+  puts "Erreur lors de l'ouverture ou de l'analyse des fichiers JSON."
+end
--- spec/available_time_slot_spec.rb
+++ spec/available_time_slot_spec.rb
@@ -1,28 +1,22 @@
 require 'rspec'
 require_relative '../available_time_slot'
 
-RSpec.describe 'Finding available slots' do
+RSpec.describe '.find_available_slots' do
+  subject { find_available_slots(serialized_data, duration) }
   let(:andy_file) { File.expand_path('../input_andy.json', __dir__) }
   let(:sandra_file) { File.expand_path('../input_sandra.json', __dir__) }
   let(:files) { [andy_file, sandra_file] }
   let(:duration) { 60 }
-  context 'find available_time_slot for agenda of Andy and Sandra' do
-    before do
-      serialized_data = serialize_data(andy_file, sandra_file)
-      @available_slots = find_available_slots(serialized_data, duration)
-    end
-    it 'finds available slots for both agendas' do
-      expect(@available_slots).to be_instance_of(Array)
-      expect(@available_slots).not_to be_empty
+  let(:serialize_data) { serialize_data(andy_file, sandra_file) }
 
-      expected_output = [
-        ['2022-08-01 14:00', '2022-08-01 15:00'],
-        ['2022-08-02 17:00', '2022-08-02 18:00'],
-        ['2022-08-04 15:00', '2022-08-04 16:00'],
-        ['2022-08-04 17:00', '2022-08-04 18:00']
-      ]
-      actual_output = @available_slots.map { |slot| slot.map { |dt| dt.strftime('%Y-%m-%d %H:%M') } }
-      expect(actual_output).to match_array(expected_output)
-    end
+  it 'should finds available slots for both agendas' do
+    expected_output = [
+      ['2022-08-01 14:00', '2022-08-01 15:00'],
+      ['2022-08-02 17:00', '2022-08-02 18:00'],
+      ['2022-08-04 15:00', '2022-08-04 16:00'],
+      ['2022-08-04 17:00', '2022-08-04 18:00']
+    ]
+    actual_output = @available_slots.map { |slot| slot.map { |dt| dt.strftime('%Y-%m-%d %H:%M') } }
+    expect(subject).to match_array(expected_output)
   end
 end
